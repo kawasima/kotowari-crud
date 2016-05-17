e212ef28e34af4715e932aa1c93c06b59f64b465
kawasima <kawasima1016@gmail.com>
2016-05-17 19:10:20 +0900

    Release 0.1.0-beta1

--- src/main/java/scaffold/crud/form/UserForm.java
+++ src/main/java/scaffold/crud/form/UserForm.java
@@ -1,10 +1,12 @@
 package scaffold.crud.form;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author kawasima
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class UserForm extends FormBase {
     private String name;
